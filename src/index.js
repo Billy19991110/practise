4f971cbc32d2f56d3f14b33cbd9b3b171361cb6d
billy1999110 <billy24681014@gmail.com>
2022-03-15 12:07:57 +0800

    0315_1207

--- src/index.js
+++ src/index.js
@@ -4,18 +4,41 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const testFunction = () => {
-  return (
-    <div>
-      <button>大家好</button>
-      <h1>我不好</h1>
-    </div>
-  );
+
+// const testFunction = () => {
+// 	return (
+// 		<div>
+// 			<button>大家好</button>
+// 			<h1>我不好</h1>
+// 		</div>
+// 	);
+// }
+// const showOne = true;
+// const styleArgument = { fontSize: '100px', color: 'red' };
+// const multiButton = () => {
+// 	var output = [];
+// 	for (let i = 0; i < 4; i++) {
+// 		output.push(<button>我是第{i}個按鍵</button>)
+// 	}
+// 	return output;
+// }
+const getValue = (event) => {
+	console.log(event.target.value)
 }
 
 ReactDOM.render(
-  testFunction(),
-  document.getElementById('root')
+	// testFunction(),
+	// <h1> {(showOne == true) ? 1 : 0} </h1>,
+	// <h1 className='title' >Hellow, world!</h1>,
+	// <div>
+	// 	{multiButton()}
+	// </div>,
+	<div>
+		<button value="true" onClick={getValue}>
+			按下以取得數值
+		</button>
+	</div>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
